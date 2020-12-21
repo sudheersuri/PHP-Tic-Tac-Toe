ee6ba747eb0023289f2b58d81dff5b98f7395691
Sudheer <sudhirsuri43@gmail.com>
2020-12-21 12:55:19 -0500

    Jquery CDN

--- home.js
+++ home.js
@@ -1,5 +1,9 @@
 var table = document.getElementById("tictable");
 
 table.addEventListener("click", function (e) {
-  e.target.innerHTML = "X";
+  //charan : take the input and generate array
+  //disable or hide respective player
+  //kartik : take this array and convert it into json format
+  //kartik : post this json data to api
+  //kartik : get the response
 });
