ab5fadd5eeafd0b13aad7ba46b2a25cbd83bdadc
Daitha, Sai Charan <Saicharan.Daitha@in.pega.com>
2020-12-22 00:42:52 +0530

    updated

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
-var table = document.getElementById("tictable");
+var table = document.getElementById("tictactable")
+var data = "123456789"
 
 table.addEventListener("click", function (e) {
   //charan : take the input and generate array
@@ -6,4 +7,11 @@ table.addEventListener("click", function (e) {
   //kartik : take this array and convert it into json format
   //kartik : post this json data to api
   //kartik : get the response
+  var p = document.getElementsByName("player")[0].checked
+  //console.log(p)
+  var i = e.target.innerHTML-1
+  var str = (p) ? "X" : "O"
+  data[i] = str
+  e.target.innerHTML = str
+  console.log(data)
 });
